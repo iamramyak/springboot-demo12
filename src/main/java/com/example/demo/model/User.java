6394c0cc25ae1c52a9f0f020fae5323f64b6acb9
iamramyak <iamramyak7@gmail.com>
2024-01-08 16:16:07 +0530

    demo project

---
+++ src/main/java/com/example/demo/model/User.java
@@ -0,0 +1,29 @@
+package com.example.demo.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+@Table
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+
+public class User {
+
+        @Id
+        @GeneratedValue
+        private Long id;
+        private String firstName;
+        private String lastName;
+        private String email;
+
+    }
+
+
